681c53c56ce928664f5181dbbee319328ea75b49
Jonathan Avinor <javinor@Jonathans-MacBook-Air.local>
2022-09-28 23:27:32 +0300

    fsharp day01 - use List.windowed

--- fsharp/day01.fsx
+++ fsharp/day01.fsx
@@ -2,24 +2,19 @@ let examplePath = "../input/day01.example"
 let realPath = "../input/day01.real"
 
 let q1 (input: string) =
-    input.Split [|'\n'|]
+    input.Split [| '\n' |]
     |> Array.toList
     |> List.map int
     |> List.pairwise
     |> List.map (fun (x, y) -> if y > x then 1 else 0)
     |> List.sum
-    
-let rec triplewise xs =
-    match xs with
-    | [] | _ :: [] | _ :: _ :: [] -> []
-    | x :: x' :: x'' :: xs -> 
-        (x, x', x'') :: (triplewise (x' :: x'' :: xs))
 
 let q2 (input: string) =
-    input.Split [|'\n'|]
+    input.Split [| '\n' |]
     |> Array.toList
     |> List.map int
-    |> triplewise |> List.map (fun (x, y, z) -> x + y + z)
+    |> List.windowed 3
+    |> List.map (List.sum)
     |> List.pairwise
     |> List.map (fun (x, y) -> if y > x then 1 else 0)
     |> List.sum
